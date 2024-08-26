6f1557970c6a2b50d1b266ce8bea93c66efd52ba
Sergey13131313 <110328066+Sergey13131313@users.noreply.github.com>
2024-08-26 12:26:55 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for fun in functions:
+        results[fun.__name__] = fun(int_list)
+    return results
+
+
+f_list = [min, max, len, sum, sorted]
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
